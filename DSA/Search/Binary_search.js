69512cab825dbf73e4fcd27ba9c731afb9351d6f
Shubham <>
2026-01-02 10:23:06 +0000

    Add DSA folder with Search algorithms

---
+++ DSA/Search/Binary_search.js
@@ -0,0 +1,20 @@
+function exists(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while(left<=right) {
+        let mid = Math.floor((left + right) / 2);
+        if(arr[mid] === target) {
+            return true;}
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false ;
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const target = 7;
+console.log(exists(arr, target));
